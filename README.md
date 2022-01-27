82c1e49ec735d590d978899cba57413c47ea681e
shanmite <shanmite@hotmail.com>
2022-01-27 17:12:25 +0800

    0.0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# 监控直播间红包
+
+新建文件`cookie.json`填入cookie
+```json
+{
+    "cookie": ""
+}
+```
+
+
+```
+$ npm start
+```
---
+++ index.js
@@ -0,0 +1,172 @@
+const { stringify } = require('querystring');
+const fetch = require("node-fetch");
+const { LiveFlow } = require("bili-live-comment-ws");
+const cookie_str = require("./cookie.json")["cookie"];
+
+/**
+ * @param {number} source_id
+ * @returns {Promise<number[]>}
+ */
+async function getAreaList(source_id = 1) {
+    const resp = await fetch(`https://api.live.bilibili.com/xlive/web-interface/v1/index/getWebAreaList?source_id=${source_id}`, {
+        method: "GET",
+        headers: {
+            cookie: cookie_str,
+            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+        }
+    })
+    const data = await resp.json()
+    if (data.code === 0) {
+        return data.data.data.map(it => it.id)
+    } else {
+        console.log(data)
+        return []
+    }
+}
+
+/**
+ * @param {number} parent_area_id
+ * @param {number} area_id
+ * @param {number} page
+ * @returns {Promise<[number,number][]>}
+ */
+async function getList(parent_area_id, area_id, page) {
+    const resp = await fetch(`https://api.live.bilibili.com/xlive/web-interface/v1/second/getList?platform=web&parent_area_id=${parent_area_id}&page=${page}&area_id=${area_id}`, {
+        method: "GET",
+        headers: {
+            cookie: cookie_str,
+            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+        }
+    })
+    const data = await resp.json()
+    if (data.code === 0) {
+        return data.data.list.map(it => [it.roomid, it.uid])
+    } else {
+        console.log(data)
+        return []
+    }
+}
+
+class CookiePaser {
+    /**
+     * @param {string} cookie
+     */
+    constructor(cookie) {
+        this.cookie = new Map(
+            cookie.split(/\s*;\s*/)
+                .map(it => it.split('='))
+        )
+    }
+
+    get(key) {
+        return this.cookie.get(key)
+    }
+}
+
+const cookie = new CookiePaser(cookie_str);
+
+class RedPacketMonitor {
+    constructor(roomid, ruid) {
+        this.room_id = roomid;
+        this.ruid = ruid;
+        this.liveflow = null;
+        this._no_relation_modify = false;
+    }
+
+    no_relation_modify() {
+        this._no_relation_modify = true;
+        return this
+    }
+
+    async start() {
+        if (await this.relation_modify(1)) {
+            this.liveflow = new LiveFlow()
+                .setCookie(cookie_str)
+                .setRoomId(this.room_id)
+                .setUid(Number(cookie.get("DedeUserID")))
+                .addCommandHandle("POPULARITY_RED_POCKET_START", (msg) => {
+                    console.log(msg);
+                    fetch("https://api.live.bilibili.com/xlive/lottery-interface/v1/popularityRedPocket/RedPocketDraw", {
+                        method: "POST",
+                        headers: {
+                            cookie: cookie_str,
+                            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+                            "content-type": "application/x-www-form-urlencoded"
+                        },
+                        body: stringify({
+                            csrf: cookie.get("bili_jct"),
+                            csrf_token: cookie.get("bili_jct"),
+                            visit_id: "",
+                            jump_from: "",
+                            session_id: "",
+                            room_id: this.room_id,
+                            ruid: this.ruid,
+                            spm_id: "444.8.red_envelope.extract"
+                        })
+                    })
+                        .then(res => res.json())
+                        .then(json => {
+                            console.log(json);
+                        });
+                });
+            this.liveflow.run()
+            return true
+        } else {
+            return false
+        }
+    }
+
+    async close() {
+        await this.relation_modify(2)
+        this.liveflow.close()
+    }
+
+    /**
+     * @param {number} act 1 关注 2 取关
+     * @return {Promise<boolean>} ok -> true
+     */
+    async relation_modify(act) {
+        if (this._no_relation_modify) {
+            console.log(`relation_modify ${act} ${this.ruid} close`);
+            return true
+        }
+        const resp = await fetch("https://api.bilibili.com/x/relation/modify", {
+            method: "POST",
+            headers: {
+                cookie: cookie_str,
+                "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+                "content-type": "application/x-www-form-urlencoded"
+            },
+            body: stringify({
+                fid: this.ruid,
+                act,
+                re_src: 0,
+                csrf: cookie.get("bili_jct")
+            })
+        })
+        const data = await resp.json()
+        if (data.code !== 0) {
+            console.log(`relation_modify ${act} ${this.ruid} error`, data);
+            return false
+        } else {
+            console.log(`relation_modify ${act} ${this.ruid} ok`);
+            return true
+        }
+    }
+}
+
+getAreaList().then(ids => {
+    ids.forEach(id => {
+        getList(id, 0, 1).then(args => {
+            args.forEach(async arg => {
+                let red_packet_monitor = new RedPacketMonitor(...arg)
+                    .no_relation_modify();
+                if (await red_packet_monitor.start()) {
+                    setTimeout(() => {
+                        red_packet_monitor.close()
+                    }, 5 * 60 * 1000);
+                }
+            })
+        })
+    })
+})
