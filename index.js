9ad9e8956d45773de6b536ac28ffce54b2270bf8
shanmite <shanmite@hotmail.com>
2022-01-31 11:41:27 +0800

    0.0.7

--- index.js
+++ index.js
@@ -1,126 +1,114 @@
 const { stringify } = require('querystring');
-const fetch = require("node-fetch");
 const { LiveFlow } = require("bili-live-comment-ws");
-const cookie_str = require("./cookie.json")["cookie"];
+const { cookie, cookies = [] } = require("./cookie.json");
+const { default: axios } = require('axios');
 
 /**
- * @param {number} source_id
- * @returns {Promise<number[]>}
- */
-async function getAreaList(source_id = 1) {
-    const resp = await fetch(`https://api.live.bilibili.com/xlive/web-interface/v1/index/getWebAreaList?source_id=${source_id}`, {
-        method: "GET",
-        headers: {
-            cookie: cookie_str,
-            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-        }
-    })
-    const data = await resp.json()
-    if (data.code === 0) {
-        return data.data.data.map(it => it.id)
-    } else {
-        console.log(data)
-        return []
-    }
-}
-
-/**
- * @param {number} page
- * @param {number} parent_area_id
- * @returns {Promise<[number,number][]>}
- */
-async function getList(page, parent_area_id) {
-    const resp = await fetch(`https://api.live.bilibili.com/xlive/web-interface/v1/second/getList?platform=web&parent_area_id=${parent_area_id}&page=${page}&area_id=0`, {
-        method: "GET",
-        headers: {
-            cookie: cookie_str,
-            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-        }
-    })
-    const data = await resp.json()
-    if (data.code === 0) {
-        return data.data.list.map(it => [it.roomid, it.uid])
-    } else {
-        console.log(data)
-        return []
-    }
-}
-
-/**
- * @returns {Promise<number[]>}
- */
-async function getAttentionList() {
-    const resp = await fetch("https://api.vc.bilibili.com/feed/v1/feed/get_attention_list", {
-        method: "GET",
-        headers: {
-            cookie: cookie_str,
-            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-        }
-    })
-    const data = await resp.json()
-    if (data.code === 0) {
-        return data.data.list || []
-    } else {
-        console.log(data)
-        return []
-    }
-}
-
-/**
- * @returns {Promise<[string,string,number]>}
- */
-async function getBagList() {
-    const resp = await fetch("https://api.live.bilibili.com/xlive/web-room/v1/gift/bag_list", {
-        method: "GET",
-        headers: {
-            cookie: cookie_str,
-            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-        }
-    })
-    const data = await resp.json()
-    if (data.code === 0) {
-        return (data.data.list || []).map(it => [it.gift_name, it.corner_mark, it.gift_num])
-    } else {
-        console.log(data)
-        return []
-    }
-}
-
-/**
- * @param {Array<Promise<any>>} jobs
+ * @template T,U
+ * @param {Array<U|Promise<U>|(param: T)=>Promise<U>|U>} jobs
  */
 async function pipe(jobs) {
     let last_value
     for (let job of jobs) {
-        last_value = await job(last_value)
+        try {
+            if (job instanceof Function) {
+                const result = job(last_value)
+                if (result instanceof Promise) {
+                    last_value = await result
+                } else {
+                    last_value = result
+                }
+            } else if (job instanceof Promise) {
+                last_value = await job
+            } else {
+                last_value = job
+            }
+        } catch (error) {
+            console.log(error.message)
+            break
+        }
+    }
+    return last_value
+}
+
+/**
+ * @param {Promise<any>[]} promises
+ */
+async function waitAll(promises) {
+    return await Promise.all(promises)
+}
+
+/**
+ * @param {string} method
+ * @param {Array<any>} [params] 
+ */
+function call(method, params = []) {
+    return (object) => object[method](...params)
+}
+
+/**
+ * @template T
+ * @param {T[]} arr
+ * @return {(a: T) => T[]}
+ */
+function push(arr) {
+    return (a) => {
+        a && arr.push(a)
+        return arr
     }
 }
 
-function setGlobal(key) {
-    return (value) => { global[key] = value }
+/**
+ * @template T
+ * @param {T} a
+ * @return {(arr: T[]) => T[]}
+ */
+function insert(a) {
+    return async (arr) => {
+        if (a instanceof Promise) {
+            a = await a
+        }
+        a && arr.push(a)
+        return arr
+    }
 }
 
 /**
- * @template T
- * @param {(data: T) => void} fn
- * @returns {(arr: Array<T>) => void}
+ * @template T,U
+ * @param {(a: T)=>U} fn
+ * @returns {(arr: T[])=>U[]}
  */
-function forEach(fn) {
-    return (arr) => arr.forEach((it) => fn(it))
+function map(fn) {
+    return (arr) => arr.map(fn)
 }
 
 /**
- * @template T
- * @template U
- * @param {T[]} first
- * @returns {(second: U[]) => [T,U][]}
+ * @template T,U
+ * @param {T[]|Promise<T[]>} second
+ * @returns {(second: U[]) => Promise<[T,U][]>}
  */
-function cross(first) {
-    return (second) => {
+function cross(second) {
+    return async (first) => {
+        if (second instanceof Promise) {
+            second = await second
+        }
         let cross = []
-        first.forEach(f => second.forEach(s => cross.push([f, s])))
+        for (const s of second) {
+            for (const f of first) {
+                cross.push([f, s])
+            }
+        }
         return cross
     }
+}
+
+/**
+ * 
+ * @param {number} depth
+ * @returns
+ */
+function flat(depth) {
+    return (arr) => arr.flat(depth)
 }
 
 /**
@@ -137,161 +125,312 @@ function list(num) {
 /**
  * @template T
  * @param {(...params)=>Promise<T>|T} fn
- * @param {(params: T)=>void} callback
- * @returns {(arr: Array)=>void}
  */
-function apply(fn, callback) {
-    return (arr) => {
+function apply(fn) {
+    return async (arr) => {
         const ret = fn(...arr)
-        if (ret.then) {
-            ret.then(callback)
+        if (ret instanceof Promise) {
+            return await ret
         } else {
-            callback(ret)
+            return ret
         }
     }
 }
 
-/**
- * 
- * @param {string} cookie_str
- * @returns {Map<string,string>}
- */
-function parseCookie(cookie_str) {
-    return new Map(
-        cookie_str.split(/\s*;\s*/)
-            .map(it => it.split('='))
-    )
+class BUser {
+    /**
+     * @param {string} cookie
+     */
+    constructor(cookie) {
+        if (cookie) {
+            this.axios = axios.create({
+                headers: {
+                    cookie,
+                    "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+                }
+            })
+            /**
+             * @type {Map<string,string>}
+             */
+            this.cookie = new Map(
+                cookie.split(/\s*;\s*/)
+                    .map(it => it.split('='))
+            )
+        } else {
+            this.axios = axios.create({
+                headers: {
+                    "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+                }
+            })
+            this.cookie = new Map()
+        }
+        /**
+         * @type {Map<string,any>}
+         */
+        this._cache = new Map()
+    }
+
+    /**
+     * @param {string} [cookie]
+     * @returns
+     */
+    static build(cookie) {
+        return new BUser(cookie)
+    }
+
+    /**
+     * @typedef Request
+     * @property {string} method
+     * @property {any[]} [params]
+     * @param {Request} request
+     */
+    get(request) {
+        return this._cache.get(request)
+    }
+
+    /**
+     * @typedef Request
+     * @property {string} method
+     * @property {any[]} [params]
+     * @param {Request} request
+     */
+    async cache(request) {
+        const result = await this[request.method](...(request.params || []))
+        this._cache.set(request, result)
+        return this
+    }
+
+    /**
+     * @param {number} source_id
+     * @returns {Promise<number[]>}
+     */
+    async getAreaList(source_id = 1) {
+        const { data } = await this.axios.get(
+            "https://api.live.bilibili.com/xlive/web-interface/v1/index/getWebAreaList",
+            {
+                params: {
+                    source_id
+                }
+            }
+        )
+        if (data.code === 0) {
+            return data.data.data.map(it => it.id)
+        } else {
+            console.log(data)
+            return []
+        }
+    }
+
+    /**
+     * @param {number} parent_area_id
+     * @param {number} page
+     * @returns {Promise<[number,number][]>} `[roomid, uid][]`
+     */
+    async getRoomListPair(parent_area_id, page) {
+        const { data } = await this.axios.get(
+            "https://api.live.bilibili.com/xlive/web-interface/v1/second/getList",
+            {
+                params: {
+                    platform: "web",
+                    parent_area_id,
+                    page,
+                    area_id: 0
+                }
+            }
+        )
+        if (data.code === 0) {
+            return data.data.list.map(it => [it.roomid, it.uid])
+        } else {
+            console.log(data)
+            return []
+        }
+    }
+
+    /**
+     * @returns {Promise<number[]>}
+     */
+    async getAttentionList() {
+        const { data } = await this.axios.get(
+            "https://api.vc.bilibili.com/feed/v1/feed/get_attention_list"
+        )
+        if (data.code === 0) {
+            return data.data.list || []
+        } else {
+            console.log(data)
+            return []
+        }
+    }
+
+    /**
+     * @returns {Promise<[string,string,number]>}
+     */
+    async getBagList() {
+        const { data } = await this.axios.get(
+            "https://api.live.bilibili.com/xlive/web-room/v1/gift/bag_list"
+        )
+        if (data.code === 0) {
+            return (data.data.list || []).map(it => [it.gift_name, it.corner_mark, it.gift_num])
+        } else {
+            console.log(data)
+            return []
+        }
+    }
+
+    /**
+     * @param {number} lot_id
+     * @param {number} room_id
+     * @param {number} ruid
+     * @returns {Promise<boolean>}
+     */
+    async drawRedPocket(lot_id, room_id, ruid) {
+        const { data } = await this.axios.post(
+            "https://api.live.bilibili.com/xlive/lottery-interface/v1/popularityRedPocket/RedPocketDraw",
+            stringify({
+                lot_id,
+                csrf: this.cookie.get("bili_jct"),
+                csrf_token: this.cookie.get("bili_jct"),
+                visit_id: "",
+                jump_from: "",
+                session_id: "",
+                room_id,
+                ruid,
+                spm_id: "444.8.red_envelope.extract"
+            }),
+            {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded"
+                }
+            }
+        )
+        if (data.code === 0) {
+            console.log('drawRedPocket', 'ok');
+            return true
+        } else {
+            console.log('drawRedPocket', 'error', data)
+            return false
+        }
+    }
+
+    /**
+     * @param {number} fid uid
+     * @param {number} act 1 关注 2 取关
+     * @return {Promise<boolean>} ok -> true
+     */
+    async relationModify(fid, act) {
+        const { data } = await this.axios.post(
+            "https://api.bilibili.com/x/relation/modify",
+            stringify({
+                fid,
+                act,
+                re_src: 0,
+                csrf: this.cookie.get("bili_jct")
+            }),
+            {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded"
+                }
+            }
+        )
+        if (data.code === 0) {
+            console.log(`relationModify ${act} ${fid}`, 'ok');
+            return true
+        } else {
+            console.log(`relationModify ${act} ${fid}`, "error", data);
+            return false
+        }
+    }
 }
 
 class RedPacketMonitor {
-    constructor(roomid, ruid) {
+    constructor(roomid, ruid, busers) {
+        /**
+         * @type {BUser[]}
+         */
+        this.busers = busers
         this.room_id = roomid;
         this.ruid = ruid;
         this.liveflow = null;
-        this._no_relation_modify = false;
         this.has_redpacket = false;
         this.timer = 0;
         this.close_time = 300000;
     }
 
-    static build(roomid, ruid) {
-        return new RedPacketMonitor(roomid, ruid)
-    }
-
-    no_relation_modify() {
-        this._no_relation_modify = true;
-        return this
+    /**
+     * @param {number} roomid
+     * @param {number} ruid
+     * @param {BUser[]} busers
+     * @returns 
+     */
+    static build(roomid, ruid, busers) {
+        return new RedPacketMonitor(roomid, ruid, busers)
     }
 
     async start() {
-        this.closeTimerUpdate()
-        if (global.attention_list.includes(this.ruid)) {
-            this.no_relation_modify()
-        }
+        this.closeTimerUpdate(this.close_time)
         this.liveflow = new LiveFlow()
-            .setCookie(cookie_str)
             .setRoomId(this.room_id)
-            .setUid(Number(global.cookie.get("DedeUserID")))
-            .addCommandHandle("POPULARITY_RED_POCKET_START", async ({ data }) => {
-                console.log(data);
-                if (!this.has_redpacket) {
-                    this.has_redpacket = true;
-                    await this.relation_modify(1)
-                }
-                if (!this._no_relation_modify) {
-                    fetch("https://api.live.bilibili.com/xlive/lottery-interface/v1/popularityRedPocket/RedPocketDraw", {
-                        method: "POST",
-                        headers: {
-                            cookie: cookie_str,
-                            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-                            "content-type": "application/x-www-form-urlencoded"
-                        },
-                        body: stringify({
-                            lot_id: data.lot_id,
-                            csrf: global.cookie.get("bili_jct"),
-                            csrf_token: global.cookie.get("bili_jct"),
-                            visit_id: "",
-                            jump_from: "",
-                            session_id: "",
-                            room_id: this.room_id,
-                            ruid: this.ruid,
-                            spm_id: "444.8.red_envelope.extract"
-                        })
-                    }).then(res => res.json()).then(res => {
-                        console.log(res)
-                        if (res.code === 0) {
-                            clearTimeout(this.timer)
-                            this.close_time = 300000 + data.last_time * 1000;
-                            this.closeTimerUpdate()
-                        }
-                    });
-                }
-                getBagList().then(console.log)
-            });
+            .addCommandHandle("POPULARITY_RED_POCKET_START", ({ data }) => {
+                console.log("POPULARITY_RED_POCKET_START", data)
+                this.has_redpacket = true
+                this.closeTimerUpdate(300000 + data.last_time * 1000)
+                this.busers.forEach(buser => {
+                    Promise.all([
+                        buser.cookie.get("DedeUserID"),
+                        buser
+                            .drawRedPocket(data.lot_id, this.room_id, this.ruid)
+                            .catch(it => it.message),
+                        buser
+                            .getBagList()
+                            .catch(it => it.message)
+                    ]).then(it => console.log(...it))
+                })
+            })
         await this.liveflow.run()
     }
 
-    closeTimerUpdate() {
-        console.log(`will disconnect in ${this.close_time}ms`);
+    closeTimerUpdate(close_time) {
+        clearTimeout(this.timer)
+        console.log(`will disconnect in ${close_time}ms`);
         this.timer = setTimeout(() => {
             this.close()
-        }, this.close_time);
+        }, close_time);
     }
 
     async close() {
-        if (this.has_redpacket) {
-            await this.relation_modify(2)
-        }
-        this.liveflow.close()
-    }
-
-    /**
-     * @param {number} act 1 关注 2 取关
-     * @return {Promise<boolean>} ok -> true
-     */
-    async relation_modify(act) {
-        if (this._no_relation_modify) {
-            console.log(`relation_modify ${act} ${this.ruid} close`);
-            return true
-        }
-        const resp = await fetch("https://api.bilibili.com/x/relation/modify", {
-            method: "POST",
-            headers: {
-                cookie: cookie_str,
-                "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            body: stringify({
-                fid: this.ruid,
-                act,
-                re_src: 0,
-                csrf: global.cookie.get("bili_jct")
+        await Promise.all(
+            this.busers.map((buser) => {
+                let attention_list = buser.get({ method: "getAttentionList" })
+                if (!attention_list.includes(this.ruid) && this.has_redpacket) {
+                    return buser
+                        .relationModify(this.ruid, 2)
+                        .catch((err) => console.log(err.message))
+                }
             })
-        })
-        const data = await resp.json()
-        if (data.code !== 0) {
-            console.log(`relation_modify ${act} ${this.ruid} error`, data);
-            return false
-        } else {
-            console.log(`relation_modify ${act} ${this.ruid} ok`);
-            return true
-        }
+        )
+        this.liveflow.close()
     }
 }
 
+const announce_buser = new BUser()
+
 pipe([
-    () => cookie_str,
-    parseCookie,
-    setGlobal('cookie'),
-    getAttentionList,
-    setGlobal('attention_list'),
-    getAreaList,
+    announce_buser.getAreaList(),
     cross(list(2)),
-    forEach(apply(
-        getList,
-        forEach(apply(
-            RedPacketMonitor.build,
-            red_packet_monitor => red_packet_monitor.start()
-        ))
-    ))
+    map(apply(announce_buser.getRoomListPair.bind(announce_buser))),
+    waitAll,
+    flat(1),
+    map(insert(
+        pipe([
+            cookie,
+            push(cookies),
+            map(BUser.build),
+            map(call("cache", [{ method: "getAttentionList" }])),
+            waitAll,
+        ])
+    )),
+    waitAll,
+    map(apply(RedPacketMonitor.build)),
+    waitAll,
+    map(call("start")),
+    waitAll
 ])
